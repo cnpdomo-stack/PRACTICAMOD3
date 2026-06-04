7d2bc7058d97118b8bd50ba67988980c3a3c0e4e
cnpdomo <cnpdomo@gmail.com>
2026-06-04 15:39:48 -0600

    cambios

---
+++ init.sql
@@ -0,0 +1,13 @@
+-- Crear tabla de usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(255) UNIQUE,
+    telefono VARCHAR(20),
+    fecha_nacimiento DATE
+);
+
+-- Crear tabla de credenciales
+CREATE TABLE IF NOT EXISTS credenciales (
+    id_credencial SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
